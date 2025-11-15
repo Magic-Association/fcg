97c97f65c27ffd823247fe10aa28216173507e69
steampunkWizard <elijahanelson2nd@gmail.com>
2025-11-15 18:12:28 -0500

    update next.js and website packages

--- apps/website/src/lib/auth-client.ts
+++ apps/website/src/lib/auth-client.ts
@@ -1,7 +1,5 @@
 import { createAuthClient } from "better-auth/client";
 
-console.log(process.env)
-
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 });
