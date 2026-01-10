f99d51a6b99b2fc67abacbd7e6e1879632633a95
Sawana <mirja@git.sawana.dev>
2026-01-10 09:24:19 +0000

    Add match lifecycle RPCs

--- apps/server/src/rpc/rpcs.ts
+++ apps/server/src/rpc/rpcs.ts
@@ -1,5 +1,12 @@
 import { createRpcRegistry } from "./registry.js";
-import { join_match, create_match, list_matches } from "./matches.js";
+import {
+  join_match,
+  create_match,
+  list_matches,
+  leave_match,
+  start_match,
+  end_match,
+} from "./matches.js";
 import { subscribe_to_lobby } from "../lobby/lobby.js";
 import { hello } from "./hello.js";
 
@@ -7,6 +14,9 @@ const rpc = createRpcRegistry()
   .define("join_match", join_match)
   .define("create_match", create_match)
   .define("list_matches", list_matches)
+  .define("leave_match", leave_match)
+  .define("start_match", start_match)
+  .define("end_match", end_match)
   .define("subscribe_to_lobby", subscribe_to_lobby)
   .define("hello", hello);
 
