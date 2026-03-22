995614c7545f9490c7cde13e1a161b487ba00c27
Sawana <sawana@git.sawana.dev>
2026-03-22 19:26:54 +0000

    Separate primitive and composed action specs

--- apps/server/src/game/engine/ActionSpec.ts
+++ apps/server/src/game/engine/ActionSpec.ts
@@ -1,9 +1,17 @@
 import { addScore } from "@actions/addScore.js";
 import { GameAction, pipe } from "./gameAction.js";
 
-export type ActionSpec =
-  | { type: "addScore"; amount: number }
-  | { type: "sequence"; actions: ActionSpec[] };
+export type PrimitiveActionSpec = {
+  type: "addScore";
+  amount: number;
+};
+
+export type ActionCompositionSpec = {
+  type: "sequence";
+  actions: ActionSpec[];
+};
+
+export type ActionSpec = PrimitiveActionSpec | ActionCompositionSpec;
 
 export const toGameAction = (spec: ActionSpec): GameAction => {
   switch (spec.type) {
