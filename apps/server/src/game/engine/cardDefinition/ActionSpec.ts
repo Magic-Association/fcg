0995bbdb35e576510135f405b5e06a4e16cc2ab7
steampunkWizard <elijahanelson2nd@gmail.com>
2026-04-19 20:44:36 -0400

    fix addCharacterScore not handling array

--- apps/server/src/game/engine/cardDefinition/ActionSpec.ts
+++ apps/server/src/game/engine/cardDefinition/ActionSpec.ts
@@ -1,6 +1,6 @@
 import { addCharacterScore } from "@actions/addCharacterScore.js";
 import { addScore } from "@actions/addScore.js";
-import { GameAction } from "../gameAction.js";
+import { GameAction, pipe } from "../gameAction.js";
 import { resolveCharacterTargets } from "./resolveCharacterTargets.js";
 import { resolveValueSpec, ValueSpec } from "./ValueSpec.js";
 import { CharacterTargetSpec } from "./TargetSpec.js";
@@ -32,8 +32,15 @@ export const toGameActions = (specs: ActionSpec[]): GameAction[] => {
       case "addCharacterScore":
         actions.push((state, context) => {
           const amount = resolveValueSpec(spec.amount, state);
-          const targetCharacterIds = resolveCharacterTargets(spec.target, state, context);
-          return addCharacterScore(targetCharacterIds, amount)(state, context);
+          const targetCharacterIds = resolveCharacterTargets(
+            spec.target,
+            state,
+            context,
+          );
+          const targetActions = targetCharacterIds.map((targetId) =>
+            addCharacterScore(targetId, amount),
+          );
+          return pipe(...targetActions)(state, context);
         });
         break;
     }
